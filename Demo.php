c35e99d194f990c77d2a2b9352274076e8cf5b7d
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-03-13 16:13:45 +0800

    -

--- Demo.php
+++ Demo.php
@@ -1,21 +1,17 @@
 <?
 require_once 'vendor/autoload.php';
 
+$del = new Jsnlib\Del;
 
-$Del = new Jsnlib\Del;
-
-$result = $Del->get("D");
-if ($result == 0) die("指定的路徑不存在!");
+$result = $del->get("D");
+if ($result === false) die("指定的路徑不存在!");
 print_r($result);
 
 //一、刪除包含自己
-// $result = $Del->all();
+// $result = $del->all();
 // if ($result) echo "清除資料夾完畢!";
 
 
 //二、刪除自己之下
-$result = $Del->under();
+$result = $del->under();
 if ($result) echo "清除資料夾完畢!";
-
-
-?>
